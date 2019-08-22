9aa36d3ad592f59abc4d4fb3dc9f5b2dfac1aa3d
DerfOh <fredrick.p@outlook.com>
2019-08-22 03:12:37 +0100

    Clean up data types
    Makes sure that everything in the api is presented to influxdb properly,
    uses GNU sed. Additionally checks that all dependencies are available
    before performing the curl.
    
    Provides a grafana dashboard example json and some
    udpates to the README dependencies section and dashboard info.

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ This repository contains a small bash script that extract a few metrics from Pi-
 - [Pi-hole][pi-hole]: It has only been tested in Pi-hole v4.0. It may require some adjustments in another versions
 - `curl` for requesting the api
 - `jq` to parse, extract and build the JSON for telegraf
+- `sed` to perform transfromations on datatypes to make them compatible with influxdb
 
 ## Installation
 - Copy the file `piholestats.sh` to `/usr/local/bin`. Make sure it has execution permission (may require `chmod +x`).
 - Edit the file `/etc/telegraf/telegraf.conf` and paste the content below:
+- (optional) import the example `grafana.json` dashboard
 
 ```
 # PiHole monitoring
--- piholestats.sh
+++ piholestats.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env sh
 
-curl http://localhost/admin/api.php | jq '.|{ domains_being_blocked, dns_queries_today, ads_blocked_today, ads_percentage_today }' 
+if [ ! -n "$(which sed)" ]; then echo "sed not installed/does not appear in $PATH"; exit 1; fi
+if [ ! -n "$(which curl)" ]; then echo "curl not installed/does not appear in $PATH"; exit 1; fi
+if [ ! -n "$(which jq)" ]; then echo "jq not installed/does not appear in $PATH"; exit 1; fi
+
+curl -s http://localhost/admin/api.php | jq '.' |
+        sed 's|"enabled"|1|g' | # changes enabled to 1 and removes quote ["status": "enabled" -> "status": 1]
+        sed 's|"disabled"|0|g' |  # changes disabled to zero and removes quote ["status": "disabled" -> "status": 0]
+        sed 's|status|enabled|g' | # changes status to enabled to better fit boolean value ["status": "enabled" -> "enabled": 1]
+        sed 's|true|1|g' | # changes true to 1 [ "file_exists": true -> "file_exists": 1]
+        sed 's|false|0|g' | # changes false to 0 [ "file_exists": false -> "file_exists": 0]
+        sed 's|"\([[:digit:]]\+\)"|\1|g' | # removes the quotes around any digits ["hours": "03" -> "hours": 03]
+        sed -r 's/0+([0-9]+)/\1/g'; # removes any leading zeros but only if there's a digit after it ["hours": 03 -> "hours": 3]
+
